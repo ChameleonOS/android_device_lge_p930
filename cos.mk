1d920c29acf5886750738649b74beabc4eb54432
Mathew Daniel <mathew.daniel@nexwave.ca>
2013-05-23 07:30:08 -0400

    Chameleoning the p930
    Change-Id: I43f1befec603687207ea7e9ac52aed0223575ff5

---
+++ cos.mk
@@ -0,0 +1,24 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cos/config/gsm.mk)
+
+TARGET_BOOTANIMATION_NAME := vertical-720x1280
+
+# Inherit some common COS stuff.
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/p930/p930.mk)
+
+PRODUCT_NAME := cos_p930
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := OptimusLTE
+PRODUCT_VERSION_DEVICE_SPECIFIC :=
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := p930
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_iproj BUILD_FINGERPRINT=lge/lge_iproj/lgp930:4.0.3/IML74K/LG-P930-V18f.1c09112307:user/release-keys PRIVATE_BUILD_DESC="i_atnt-user 4.0.3 IML74K 47c54a96 release-keys"
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch WiFiDirectDemo
